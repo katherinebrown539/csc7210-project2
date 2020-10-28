59ab2618fc47be5d0a41ccb357df84e66e51d17d
thekatiebr <katherine.e.brown.68@gmail.com>
2020-10-28 09:00:54 -0500

    custom vgg16-like architecture

--- source/ModelPrototype.py
+++ source/ModelPrototype.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Conv2D, Dropout, MaxPooling2D, Dense
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
@@ -14,21 +14,47 @@ import DiabetesData
 train_generator, val_generator, test_generator = DiabetesData.getDataGenerators(data_file="data/trainLabels.csv")
 batch_size = 16
 epochs = 1
-#generate model
-base_model = keras.applications.VGG16(weights="imagenet", include_top=False)
-x = base_model.output
-# let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- let's say we have 200 classes
-predictions = Dense(2, activation='softmax')(x)
 
-# this is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
+model = Model.Sequential()
+        
+model.add(Conv2D(32, 7, strides=(2,2), activation="relu", input_shape=input_shape_))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Conv2D(32, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Conv2D(64, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Conv2D(128, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Conv2D(256, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(256, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(256, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(Conv2D(256, 3, strides=(1,1), activation="relu"))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dense(1024, activation="relu"))
+model.add(Dropout(0.25))
+model.add(Dense(1024, activation="relu"))
+model.add(Dense(2, activation="softmax"))
 
-# first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional VGG16 layers
-for layer in base_model.layers:
-    layer.trainable = False
+
+
 
 model.compile(loss="binary_crossentropy", optimizer='adam', metrics=['accuracy'])
 
