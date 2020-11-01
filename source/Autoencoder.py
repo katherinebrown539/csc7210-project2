d149070713cc2c8fe841ff21713c7b8d612f354a
thekatiebr <katherine.e.brown.68@gmail.com>
2020-11-01 08:02:07 -0600

    deep ae

--- source/Autoencoder.py
+++ source/Autoencoder.py
@@ -17,17 +17,23 @@ class ConvAutoencoder(nn.Module):
     def __init__(self, device="cpu"):
         super(ConvAutoencoder, self).__init__()
         ## encoder layers ##
-        # conv layer (depth from 3 --> 16), 3x3 kernels
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)  
-        # conv layer (depth from 16 --> 4), 3x3 kernels
-        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+
+        #3 -> 128 -> 64 -> 32 -> 16 -> 4
+        self.conv1 = nn.Conv2d(3, 128, 3, padding=1)  
+        self.conv2 = nn.Conv2d(128, 64, 3, padding=1)  
+        self.conv3 = nn.Conv2d(64, 32, 3, padding=1)  
+        self.conv4 = nn.Conv2d(32, 16, 3, padding=1)  
+        self.conv4 = nn.Conv2d(16, 4, 3, padding=1)  
+        
         # pooling layer to reduce x-y dims by two; kernel and stride of 2
         self.pool = nn.MaxPool2d(2, 2)
         
         ## decoder layers ##
         ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
         self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(16, 32, 2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(32, 64, 2, stride=2)
+        self.t_conv4 = nn.ConvTranspose2d(64, 128, 2, stride=2)
         
         self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
         self.criterion = nn.MSELoss() # nn.BCELoss()
@@ -42,13 +48,21 @@ class ConvAutoencoder(nn.Module):
         x = self.pool(x)
         # add second hidden layer
         x = F.relu(self.conv2(x))
-        x = self.pool(x)  # compressed representation
+        x = self.pool(x)  
+        # third hidden layer
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        # fourth hidden layer
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
         
         ## decode ##
         # add transpose conv layers, with relu activation function
         x = F.relu(self.t_conv1(x))
         # output layer (with sigmoid for scaling from 0 to 1)
         x = F.relu(self.t_conv2(x))
+        x = F.relu(self.t_conv3(x))
+        x = F.relu(self.t_conv4(x))
                 
         return x
 
