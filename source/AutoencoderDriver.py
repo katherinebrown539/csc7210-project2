56261a2a417b46bd8528281c3be01b4e70710148
thekatiebr <katherine.e.brown.68@gmail.com>
2020-11-05 14:43:30 -0600

    Fix

--- source/AutoencoderDriver.py
+++ source/AutoencoderDriver.py
@@ -22,7 +22,7 @@ from ConvVarAutoencoder import ConvVAE
 from DogCatData import DogCatData
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-datatype="dogcat"
+datatype="diabetes"
 batch_size=16
 epochs = 1
 
