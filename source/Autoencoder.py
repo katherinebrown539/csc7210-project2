6b255a27566089106635543ebae92e4ec8f1976f
thekatiebr <katherine.e.brown.68@gmail.com>
2020-11-05 13:48:00 -0600

    There we go

--- source/Autoencoder.py
+++ source/Autoencoder.py
@@ -18,24 +18,24 @@ class ConvAutoencoder(nn.Module):
         super(ConvAutoencoder, self).__init__()
         ## encoder layers ##
 
-    self.encoder_layers = nn.ModuleList([
-            nn.Conv2d(3, 256, 3, padding=1),
+        self.encoder_layers = nn.ModuleList([
+                nn.Conv2d(3, 256, 3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(256, 4, 3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2)
+            ])
+
+        
+            #Decoder
+        self.decoder_layers = nn.ModuleList([
+            nn.ConvTranspose2d(4, 256, 2, stride=2), 
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(256, 4, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.ConvTranspose2d(256, 3, 2, stride=2),
+            nn.Sigmoid()
         ])
-
-       
-        #Decoder
-    self.decoder_layers = nn.ModuleList([
-        nn.ConvTranspose2d(4, 256, 2, stride=2), 
-        nn.ReLU(),
-        nn.MaxPool2d(2, 2),
-        nn.ConvTranspose2d(256, 3, 2, stride=2),
-        nn.Sigmoid()
-    ])
         
         self.to(device)
         self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
