097d07babb9c6022bd4508bf3e58cd49f3ad86b8
thekatiebr <katherine.e.brown.68@gmail.com>
2020-11-06 12:27:08 -0600

    Fixes

--- source/AutoencoderDriver.py
+++ source/AutoencoderDriver.py
@@ -31,7 +31,7 @@ size=100
 
 if datatype == "diabetes":
     filename = "data/trainLabels.csv"
-    root_dir = "data/diabetes"
+    root_dir = "data/diabetes_original"
     task = ([0,1,2], [3,4])
     # task = ([0,1,2], (3,4))
 
@@ -40,7 +40,7 @@ if datatype == "diabetes":
     train, test = train_test_split(data, test_size=0.1)
     train, val = train_test_split(train, test_size=0.1)
 
-    train = train[train["level"] < 2]
+    train = train[train["level"] < 3]
     print(train)
     classes = ['none', 'severe']
     #filter out 1s from training set
